a05274076f1af5cf76164f7f30af135a6c19cfb4
bigSam <uelsam994@gmail.com>
2025-09-09 11:06:02 +0100

    client folder created for the new frontend ui

--- client/src/components/commonUI/Button.jsx
+++ client/src/components/commonUI/Button.jsx
@@ -7,7 +7,7 @@ const Button = ({ children, type = "button", props }) => {
     <button
       type={type}
       {...props}
-      className="bg-radial cursor-pointer from-[#ff6f61] to-[#ff6e6167] p-4 hover:from-[#e75648] text-base text-center transition-colors tracking-wider duration-500 rounded-full shadow-2xl w-full"
+      className="bg-radial cursor-pointer from-[#ff6f61] to-[#ff6e6167] p-3 hover:from-[#e75648] text-base text-center transition-colors tracking-wider duration-500 rounded-full shadow-2xl w-full"
     >
       {children}
     </button>
